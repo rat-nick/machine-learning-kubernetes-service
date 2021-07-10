4373dba88d878ea7d91689a175f48045a2b942a2
ratinac-nikola <ratinac.nikola@gmail.com>
2021-07-10 18:58:26 +0200

    persistence microservice
    defined deployment
    defined service as ClusterIP
    defined dockerfile for building images
    defined requirements
    implemented model persistence with pickledb as key, value store

--- microservices/persistence/service.py
+++ microservices/persistence/service.py
@@ -9,19 +9,29 @@ db = pickledb.load("models.db", False)
 @app.route("/", methods=['POST'])
 def save():
     key = str(uuid.uuid1())
+    
     model = request.args['model']
-    db.set(key, model)
+    accuracy = request.args['accuracy']
+    auc = request.args['auc']
+    data = {
+        'model': model,
+        'accuracy': accuracy,
+        'auc' : auc
+    }
+    db.set(key, data)
+    
     return {
         "key": key,
-        "model" : model
+        "data" : data,
     }
 
 @app.route("/<model_id>", methods=['GET'])
 def load(model_id):
     
-    model = db.get(model_id)
+    data = db.get(model_id)
     return {
-        "model" : model
+        "model_id": model_id,
+        "data" : data
     }
 
 if __name__ == "__main__":
